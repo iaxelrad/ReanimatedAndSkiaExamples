6e87323d8e96fcf9545639783e88514fb1e7ad56
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-10 15:23:08 +0200

    The 3D Transform

--- App.tsx
+++ App.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+import BackgroundGradient from './components/BackgroundGradient';
 
-const App = () => {
-  return <View style={styles.container} />;
-};
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
+const HEIGHT = 256;
+const WIDTH = SCREEN_WIDTH * 0.9;
+
+const CARD_HEIGHT = HEIGHT - 5;
+const CARD_WIDTH = WIDTH - 5;
+
+function App() {
+  const gesture = Gesture.Pan();
+
+  return (
+    <View style={styles.container}>
+      <BackgroundGradient width={WIDTH} height={HEIGHT} />
+      <GestureDetector gesture={gesture}>
+        <Animated.View
+          style={{
+            height: CARD_HEIGHT,
+            width: CARD_WIDTH,
+            backgroundColor: 'black',
+            position: 'absolute',
+            borderRadius: 20,
+            zIndex: 300,
+            transform: [
+              {
+                perspective: 300,
+              },
+              {rotateX: '10deg'},
+              {rotateY: '-10deg'},
+            ],
+          }}
+        />
+      </GestureDetector>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black',
   },
 });
 
-export default App;
+export default () => {
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <App />
+    </GestureHandlerRootView>
+  );
+};
