4932d86f3c0eb43c5f3394921457a215b32a5d10
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-11 15:34:05 +0200

    Renamed App

--- android/app/src/main/java/com/reanimatedandskiaexamples/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/reanimatedandskiaexamples/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.animated3dcard.newarchitecture.components;
+package com.reanimatedandskiaexamples.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
--- android/app/src/main/java/com/reanimatedandskiaexamples/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/reanimatedandskiaexamples/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.animated3dcard.newarchitecture.modules;
+package com.reanimatedandskiaexamples.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
@@ -41,7 +41,7 @@ public class MainApplicationTurboModuleManagerDelegate
     if (!sIsSoLibraryLoaded) {
       // If you change the name of your application .so file in the Android.mk file,
       // make sure you update the name here as well.
-      SoLoader.loadLibrary("animated3dcard_appmodules");
+      SoLoader.loadLibrary("reanimatedandskiaexamples_appmodules");
       sIsSoLibraryLoaded = true;
     }
   }
