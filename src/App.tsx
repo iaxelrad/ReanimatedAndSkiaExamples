84eb5dad74419b19e7a03e325ee3466f4fbab278
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-13 16:16:50 +0200

    Setup Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Switch} from 'react-native';
+
+const Colors = {
+  dark: {
+    background: '#1E1E1E',
+    circle: '#252525',
+    text: '#F8F8F8',
+  },
+  light: {
+    background: '#F8F8F8',
+    circle: '#FFF',
+    text: '#1E1E1E',
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: 'rgba(256, 0, 256, 0.2)',
+  false: 'rgba(0,0,0, 0.1)',
+};
+
+type Theme = 'light' | 'dark';
 
 function App() {
+  const [theme, setTheme] = useState<Theme>('light');
   return (
     <View style={styles.container}>
-      <Text>Reanimated And Skia Examples</Text>
+      <Switch
+        value={theme === 'dark'}
+        onValueChange={toggled => {
+          setTheme(toggled ? 'dark' : 'light');
+        }}
+        trackColor={SWITCH_TRACK_COLOR}
+        thumbColor={'violet'}
+      />
     </View>
   );
 }
