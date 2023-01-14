7ca7a075655c7ceb4f845f18d2b7ec294ee7b7c5
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-14 21:22:36 +0200

    Added react native navigation packages

--- android/app/src/main/java/com/reanimatedandskiaexamples/MainActivity.java
+++ android/app/src/main/java/com/reanimatedandskiaexamples/MainActivity.java
@@ -1,4 +1,5 @@
 package com.reanimatedandskiaexamples;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Reanimated and Skia Examples";
+  }
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
