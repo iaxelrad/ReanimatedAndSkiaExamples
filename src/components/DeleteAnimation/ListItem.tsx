0503b849c98fd9f11092631f92b3bc49b133da3a
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-15 20:04:20 +0200

    The missing icon

--- src/components/DeleteAnimation/ListItem.tsx
+++ src/components/DeleteAnimation/ListItem.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {
-  Gesture,
-  GestureDetector,
-  GestureHandlerRootView,
+  // Gesture,
+  // GestureDetector,
+  // GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
 } from 'react-native-gesture-handler';
 import Animated, {
+  useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import {TaskInterface} from '.';
 
 type ListItemProps = {
@@ -18,13 +22,23 @@ type ListItemProps = {
 
 const ListItem = ({task}: ListItemProps) => {
   const translateX = useSharedValue(0);
-  const gesture = Gesture.Pan()
-    .onStart(event => {
+
+  // const gesture = Gesture.Pan()
+  //   .onStart(event => {
+  //     translateX.value = event.translationX;
+  //   })
+  //   .onEnd(() => {
+  //     translateX.value = withTiming(0);
+  //   });
+
+  const gesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+    onActive: event => {
       translateX.value = event.translationX;
-    })
-    .onEnd(() => {
+    },
+    onEnd: () => {
       translateX.value = withTiming(0);
-    });
+    },
+  });
 
   const rStyle = useAnimatedStyle(() => {
     return {
@@ -36,14 +50,30 @@ const ListItem = ({task}: ListItemProps) => {
     };
   });
 
+  // return (
+  //   <View style={styles.taskContainer}>
+  //     <View style={styles.iconContainer}>
+  //       <Icon name={'trash-alt'} size={70 * 0.4} color={'red'} />
+  //     </View>
+  //     <GestureDetector gesture={gesture}>
+  //       <Animated.View style={[styles.task, rStyle]}>
+  //         <Text style={styles.taskTitle}>{task.title}</Text>
+  //       </Animated.View>
+  //     </GestureDetector>
+  //   </View>
+  // );
+
   return (
-    <GestureHandlerRootView style={styles.taskContainer}>
-      <GestureDetector gesture={gesture}>
+    <View style={styles.taskContainer}>
+      <View style={styles.iconContainer}>
+        <Icon name={'trash-alt'} size={70 * 0.4} color={'red'} />
+      </View>
+      <PanGestureHandler onGestureEvent={gesture}>
         <Animated.View style={[styles.task, rStyle]}>
           <Text style={styles.taskTitle}>{task.title}</Text>
         </Animated.View>
-      </GestureDetector>
-    </GestureHandlerRootView>
+      </PanGestureHandler>
+    </View>
   );
 };
 
@@ -53,13 +83,13 @@ const styles = StyleSheet.create({
   taskContainer: {
     width: '100%',
     alignItems: 'center',
+    marginVertical: 10,
   },
   task: {
     width: '90%',
     height: 70,
-    marginVertical: 10,
     justifyContent: 'center',
-    paddingLeft: 20,
+    paddingStart: 20,
     backgroundColor: 'white',
     borderRadius: 10,
     shadowOpacity: 0.08,
@@ -71,6 +101,14 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   taskTitle: {
-    fontSize: 16,
+    fontSize: 14,
+  },
+  iconContainer: {
+    height: 70,
+    width: 70,
+    position: 'absolute',
+    right: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
